0cb912410573505e8e6e02a28b4ea653ef194403
tanishqiiitd <132388410+tanishqiiitd@users.noreply.github.com>
2023-05-15 20:29:37 +0530

    A few updates on the code

--- assembler_try.py
+++ assembler_try.py
@@ -1,4 +1,4 @@
-opcode = {  
+opcode = {
   "add": '00000',
   "sub": '00001',
   "mov": '00010',
@@ -31,6 +31,9 @@ register = {
   "R6": '110',
   "FLAGS": '111'
 }
+
+f2 = open("output.txt", "w")
+
 #holds label declaration line number
 labeldec = {}
 #holds label usage line number
@@ -64,8 +67,8 @@ wordslist = words.split("\n")
 for i in wordslist:
   temp = i.split(" ")
   wordsfinal.append(temp)
-  
-  #error for variables
+
+#error for variables
 vartest = 0
 insttest = 0
 
@@ -80,6 +83,7 @@ for i in wordsfinal:
 
 if vartest > insttest:
   print("error: Variables not declared in the beginning")
+  f2.write("error: Variables not declared in the beginning" + "\n")
   errortest = 1
 
 #assigning memory to variables
@@ -124,8 +128,11 @@ for i in wordsfinal:
   if i[0] == "mov":
     if i[2][0] == "$":
       if i[1] not in register:
-        print("error in line", opcodevar + 1 + len(memory), ": Register not found.")
-        errortest=1
+        print("error in line", opcodevar + 1 + len(memory),
+              ": Register not found.")
+        f2.write("error in line " + str(opcodevar + 1 + len(memory)) +
+                 ": Register not found." + "\n")
+        errortest = 1
         break
       binarycode.append("00010")
 
@@ -133,7 +140,10 @@ for i in wordsfinal:
       binarycode[opcodevar] = binarycode[opcodevar] + register[i[1]]
       immvalue = binaryconvert(int(i[2][1::]))
       if len(immvalue) > 7:
-        print("error in line", opcodevar + 1 + len(memory), ": Illegal immediate value.")
+        print("error in line", opcodevar + 1 + len(memory),
+              ": Illegal immediate value.")
+        f2.write("error in line" + str(opcodevar + 1 + len(memory)) +
+                 ": Illegal immediate value.")
         errortest = 1
         break
       binarycode[opcodevar] = binarycode[opcodevar] + immvalue
@@ -146,9 +156,10 @@ for i in wordsfinal:
     if i[0][-1] == ":":
       continue
     print("error in line", opcodevar + 1 + len(memory), ": Incorrect opcode.")
+    f2.write("error in line " + str(opcodevar + 1 + len(memory)),
+             ": Incorrect opcode.")
     errortest = 1
     break
-    
 
   else:
     binarycode.append(opcode[i[0]])
@@ -169,7 +180,10 @@ for i in wordsfinal:
   #A
   if i[0] in listA:
     if i[1] not in register or i[2] not in register or i[3] not in register:
-      print("error in line", opcodevar + 1 + len(memory), ": Register not found.")
+      print("error in line", opcodevar + 1 + len(memory),
+            ": Register not found.")
+      f2.write("error in line " + str(opcodevar + 1 + len(memory)) +
+               ": Register not found.")
       errortest = 1
       break
     binarycode[opcodevar] = binarycode[opcodevar] + "00"  #unused bits
@@ -180,22 +194,31 @@ for i in wordsfinal:
   #B
   if i[0] in listB:
     if i[1] not in register:
-      print("error in line", opcodevar + 1 + len(memory), ": Register not found.")
+      print("error in line", opcodevar + 1 + len(memory),
+            ": Register not found.")
+      f2.write("error in line " + str(opcodevar + 1 + len(memory)) +
+               ": Register not found.")
       errortest = 1
       break
     binarycode[opcodevar] = binarycode[opcodevar] + "0"  #unused bits
     binarycode[opcodevar] = binarycode[opcodevar] + register[i[1]]
     immvalue = binaryconvert(int(i[2][1::]))
     if len(immvalue) > 7:
-      print("error in line", opcodevar + 1 + len(memory), ": Illegal immediate value.")
+      print("error in line", opcodevar + 1 + len(memory),
+            ": Illegal immediate value.")
+      f2.write("error in line" + str(opcodevar + 1 + len(memory)) +
+               ": Illegal immediate value.")
       errortest = 1
       break
     binarycode[opcodevar] = binarycode[opcodevar] + immvalue
-    
+
   #C
   if i[0] in listC:
     if i[1] not in register or i[2] not in register:
-      print("error in line", opcodevar + 1 + len(memory), ": Register not found.")
+      print("error in line", opcodevar + 1 + len(memory),
+            ": Register not found.")
+      f2.write("error in line " + str(opcodevar + 1 + len(memory)) +
+               ": Register not found.")
       errortest = 1
       break
     binarycode[opcodevar] = binarycode[opcodevar] + "00000"  #unused bits
@@ -205,12 +228,17 @@ for i in wordsfinal:
   #D
   if i[0] in listD:
     if i[1] not in register:
-      print("error in line", opcodevar + 1 + len(memory), ": Register not found.")
+      print("error in line", opcodevar + 1 + len(memory),
+            ": Register not found.")
+      f2.write("error in line " + str(opcodevar + 1 + len(memory)) +
+               ": Register not found.")
       errortest = 1
       break
     if i[2] not in memory:
       print("error: Memory not found in line " +
             str(opcodevar + 1 + len(memory)))
+      f2.write("error: Memory not found in line " +
+               str(opcodevar + 1 + len(memory)))
       errortest = 1
       break
     binarycode[opcodevar] = binarycode[opcodevar] + "0"  #unused bits
@@ -220,8 +248,11 @@ for i in wordsfinal:
   #E
   if i[0] in listE:
     if (i[1] + ":") not in labeldec:
-      print("error in line", opcodevar + 1 + len(memory), ": Use of undefined label.")
-      errortest=1
+      print("error in line", opcodevar + 1 + len(memory),
+            ": Use of undefined label.")
+      f2.write("error in line " + str(opcodevar + 1 + len(memory)) +
+               ": Use of undefined label.")
+      errortest = 1
       break
     binarycode[opcodevar] = binarycode[opcodevar] + "0000"  #unused bits
     binarycode[opcodevar] = binarycode[opcodevar] + binaryconvert(
@@ -244,17 +275,17 @@ for i in wordsfinal:
       break
 if hlttest == 0:
   print("error: Missing halt instruction.")
+  f2.write("error: Missing halt instruction.")
+  errortest = 1
 elif hlttest == 1:
   if wordsfinal[-1] != ['hlt']:
     print("error: Halt not used as the last instruction.")
+    f2.write("error: Halt not used as the last instruction.")
     errortest = 1
-
-f2 = open("output.txt", "w")
 
 if errortest == 0:
   for i in binarycode:
     f2.write(i + "\n")
-    print(i)
 
 f2.close()
 f.close()
